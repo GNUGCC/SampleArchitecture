c1b92f2701e9df8b351d18c37b24919a3ae02a6b
JeffreyLiu <>
2026-03-04 16:51:16 +0800

    init

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace TestProject1
+{    
+    public class Tests
+    {
+        static void Main(string[] args)
+        {
+            var test = new Tests();
+            test.Setup();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            var builder = Host.CreateDefaultBuilder()
+                .ConfigureHostConfiguration(x => x.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false))
+                .Build();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
