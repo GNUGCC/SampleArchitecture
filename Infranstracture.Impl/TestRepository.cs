11ff64a77af3a4d5361eb58b2c2d50995dc7ce52
JeffreyLiu <>
2026-03-08 03:08:46 +0800

    add delivery services

--- Infranstracture.Impl/TestRepository.cs
+++ Infranstracture.Impl/TestRepository.cs
@@ -3,6 +3,15 @@ using Domain.Account;
 
 namespace Infranstracture.Test;
 
-readonly struct TestRepository : IMenuRepository, IAccountRepository
+public readonly struct TestRepository : IMenuRepository, IAccountRepository
 {
+    Task<string[]> IMenuRepository.QueryMenu()
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<MenuConfigure> IMenuRepository.QueryMenuConfigure()
+    {
+        throw new NotImplementedException();
+    }
 }
