6073b8dbcb54732072ca1398b8af168a396c9e93
JeffreyLiu <>
2026-03-13 14:57:10 +0800

    init
    # Conflicts:
    #	Database.Application/Cart/CartLogic.cs

---
+++ Database.Application/Cart/CartLogic.cs
@@ -0,0 +1,16 @@
+﻿using Application.Interface.Cart;
+
+namespace Application.Impl.Cart;
+
+readonly struct CartLogic : ICart
+{
+    Task<bool> ICart.AddOrder()
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<CartConfigure> ICart.QueryCartConfigure()
+    {
+        throw new NotImplementedException();
+    }
+}
